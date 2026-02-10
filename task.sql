ab973145c3dfaecda7b37cd1115964cf7f8e0194
dima.yeromin <dima.yeromin@gmail.com>
2026-02-10 23:37:48 +0100

    done

--- task.sql
+++ task.sql
@@ -3,6 +3,7 @@ USE ShopDB;
 
 DELIMITER $$
 
+DROP PROCEDURE IF EXISTS get_warehouse_product_inventory $$
 CREATE PROCEDURE get_warehouse_product_inventory(IN warehouse_id INT)
 BEGIN
     SELECT p.Name, pi.WarehouseAmount
